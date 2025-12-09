c89b315248b9713aad3fe922e02e2f6d2235d90e
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2025-12-09 16:53:44 +0100

    add comments to source generator entry point

--- source/Cloudbb/Cloudbb.Web/Data/ApplicationModelLoader.cs
+++ source/Cloudbb/Cloudbb.Web/Data/ApplicationModelLoader.cs
@@ -2,5 +2,11 @@
 
 namespace Cloudbb.Web.Data;
 
+/// <summary>
+/// Discovers the Entity Framework Core entity models defined in the Cloudbb.Web assembly during compilation and emits
+/// a source-generated <see cref="IModelLoader"/> implementation for loading them into an EF Core <see cref="DbContext"/>.
+/// </summary>
+// see https://github.com/WKG-Software-GmbH/wkg-entity-framework-core/blob/main/docs/documentation.md#source-generator-discovery
+// TODO: .NET 10 release not yet mirrored to GitHub.
 [ModelLoader(AssemblyDiscoveryFailureBehavior = AssemblyDiscoveryFailureBehavior.Error, TargetAssemblies = ["Cloudbb.Web"])]
 internal sealed partial class ApplicationModelLoader;
