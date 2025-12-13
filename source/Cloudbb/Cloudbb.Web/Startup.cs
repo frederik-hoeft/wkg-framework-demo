8116c29207596c135352f0a1663c307ad09e5497
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2025-12-13 16:32:19 +0100

    move CORS settings to config file

--- source/Cloudbb/Cloudbb.Web/Startup.cs
+++ source/Cloudbb/Cloudbb.Web/Startup.cs
@@ -8,6 +8,7 @@ using Cloudbb.Web.Services.Auth.Policies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.Data;
 using System.Text.Json;
@@ -104,18 +105,15 @@ internal sealed class Startup : IAsyncStartupScript
             options.JsonSerializerOptions.PropertyNamingPolicy = namingPolicy;
             options.JsonSerializerOptions.WriteIndented = true;
         });
-        
+
         // Add CORS support for Blazor client
-        services.AddCors(options =>
-        {
-            options.AddPolicy("BlazorClient", policy =>
-            {
-                policy.WithOrigins("https://localhost:7089", "http://localhost:5097")
-                      .AllowAnyMethod()
-                      .AllowAnyHeader()
-                      .AllowCredentials();
-            });
-        });
+        string[] allowedOrigins = configuration.GetValue<string[]>("CORS:AllowedOrigins")
+            ?? throw new InvalidOperationException("CORS:AllowedOrigins configuration is missing.");
+        services.AddCors(options => options.AddPolicy("BlazorClient", policy => policy
+            .WithOrigins(allowedOrigins)
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()));
         services.AddApiVersioning(options =>
         {
             options.AssumeDefaultVersionWhenUnspecified = true;
